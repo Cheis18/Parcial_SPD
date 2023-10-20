bba6712bdf8165d7c34f001378d3350acd71a7de
Cheis18 <113544459+Cheis18@users.noreply.github.com>
2023-10-20 19:20:03 -0300

    Update Parcial3_SPD.cpp

--- Parcial3_SPD.cpp
+++ Parcial3_SPD.cpp
@@ -3,7 +3,7 @@
 //SPD - Primer parcial - Parte 3
 
 //------------------------------------
-//reemplazo valores por nombres especificos sin usar espacio de memoria
+// Reemplazo valores por nombres específicos sin usar espacio de memoria
 #define PULSADOR_RESTA 2
 #define PULSADOR_SUMA 3
 #define INTERRUPTOR 4
@@ -23,46 +23,41 @@
 #define ANTIREBOTE 10
 
 #define SENSOR_FLEXION A0
-#define FOTODIODO A3
+#define SENSOR_LUZ A3
 
 //------------------------------------
-//Defino variables
+// Defino variables
 int contador = 0;
 int primo = 0;
 int timer = 0;
 
-// flags para cada estado. 1 seria "no presionado" y 0 "seria presionado"
-
+// Flags para cada estado. 1 sería "no presionado" y 0 sería "presionado"
 int suma = 1;
 int resta = 1;
 
-/* flags que guardan el estado anterior de los botones
-para comparar y determinar si se presionó el botón*/
-
+/* Flags que guardan el estado anterior de los botones
+para comparar y determinar si se presionó el botón */
 int suma_anterior = 1;
 int resta_anterior = 1;
 
-//Sensor de flexión
+// Sensor de flexión
 int lectura_flexion;
 int angulo;
-int angulo_anterior = -1; //flag que guarda el estado anterior
+int angulo_anterior = -1; // Flag que guarda el estado anterior
 
-//Fotodiodo
+// Sensor de luz ambiental
 int lectura_luz;
 int luz;
-int luz_anterior = -1; //flag que guarda el estado anterior
+int luz_anterior = -1; // Flag que guarda el estado anterior
 
 //------------------------------------
-//Establezco la configuración principal
-void setup()
-{
+// Establezco la configuración principal
+void setup() {
   pinMode(PULSADOR_RESTA, INPUT_PULLUP);
   pinMode(PULSADOR_SUMA, INPUT_PULLUP);
   pinMode(INTERRUPTOR, INPUT_PULLUP);
-
   pinMode(UNIDAD, OUTPUT);
   pinMode(DECENA, OUTPUT);
-
   pinMode(A, OUTPUT);
   pinMode(B, OUTPUT);
   pinMode(C, OUTPUT);
@@ -70,63 +65,54 @@ void setup()
   pinMode(E, OUTPUT);
   pinMode(F, OUTPUT);
   pinMode(G, OUTPUT);
- 
-  pinMode(A0, INPUT);
-  pinMode(A3, INPUT);  
+  pinMode(SENSOR_FLEXION, INPUT);
+  pinMode(SENSOR_LUZ, INPUT);  // Configura A3 como entrada analógica
   Serial.begin(9600);
 }
 
 //------------------------------------
-//declaro la función bucle principal
-void loop()
-{
-  controlarLuz();//llamo a la función controlarLuz(); para que entre en funcionamiento el fotodiodo
-  mostrarAngulo();//llamo a la función mostrarAngulo() para que entre en funcionamiento el sensor de flexión
+// Declaro la función bucle principal
+void loop() {
+  controlarLuz();  // Llamo a la función controlarLuz() para que entre en funcionamiento el sensor de luz
+  mostrarAngulo();  // Llamo a la función mostrarAngulo() para que entre en funcionamiento el sensor de flexión
 
-  /*declaro la variable valorLectura que guarda el valor del INTERRUPTOR*/  
+  // Declaro la variable valorLectura que guarda el valor del INTERRUPTOR
   int valorLectura = digitalRead(INTERRUPTOR);
 
-  if(!valorLectura)//Si el interruptor retorna el valor "0"...
+  if (!valorLectura)  // Si el interruptor retorna el valor "0"...
   {
-    /*declaro la variable pulsador_presionado que guarda 
-    lo que me devuelva la función presionarPulsador()*/
+    // Declaro la variable pulsador_presionado que guarda lo que me devuelva la función presionarPulsador()
     int pulsador_presionado = teclaPresionada();
 
-    if(pulsador_presionado == PULSADOR_SUMA)//Si el PULSADOR_SUMA fue presionado
+    if (pulsador_presionado == PULSADOR_SUMA)  // Si el PULSADOR_SUMA fue presionado
     {
-      contador++;//aumento el valor del contador sumando +1.
-      if(contador > 99)//si el contador se vuelve mayor a 99, seteo el valor del cont. a 0
-      contador = 0;
-    }
-    else if(pulsador_presionado == PULSADOR_RESTA)//Si el PULSADOR_RESTA fue presionado
+      contador++;  // Aumento el valor del contador sumando +1.
+      if (contador > 99)  // Si el contador se vuelve mayor a 99, seteo el valor del cont. a 0
+        contador = 0;
+    } else if (pulsador_presionado == PULSADOR_RESTA)  // Si el PULSADOR_RESTA fue presionado
     {
-      contador--;//disminuyo el valor del contador restando -1.
-      if(contador < 0)//si el contador se vuelve menor a 0, seteo el valor del cont. a 99
-      contador = 99;
+      contador--;  // Disminuyo el valor del contador restando -1.
+      if (contador < 0)  // Si el contador se vuelve menor a 0, seteo el valor del cont. a 99
+        contador = 99;
     }
     mostrarContador(contador);
   }
-  if (valorLectura)//Si el interruptor retorna el valor "1"...
+  if (valorLectura)  // Si el interruptor retorna el valor "1"...
   {
-    /*Al usar un delay, los visualizadores empiezan a funcionar de manera defectuosa,
+    /* Al usar un delay, los visualizadores empiezan a funcionar de manera defectuosa,
     por lo que la solución que le encontré es crear un contador (timer) para que no afecte
-    el funcionamiento de la función mostrarContador(primo)*/
-
-    timer++;//aumento el timer
-    if(timer==20)//si el timer llega al valor "20", entra el if...
+    el funcionamiento de la función mostrarMultiplos5(contador) */
+    timer++;  // Aumento el timer
+    if (timer == 20)  // Si el timer llega al valor "20", entra el if...
     {
-      timer=0;//el timer se reinicia
-      contador++;//incrementa el valor del contador (actuando así de manera automatica)
-      if(detectarPrimo(contador))//Si el valor del contador es primo...
+      timer = 0;  // El timer se reinicia
+      contador++;  // Incrementa el valor del contador (actuando así de manera automática)
+      mostrarMultiplos5(contador);  // Muestra en los visualizadores, el múltiplo de 5 más cercano al contador
+      if (contador > 99)  // Si el contador es mayor a 99...
       {
-        primo = contador;//la variable primo guarda el número del contador
-      }
-      if(contador>99)//Si el contador es mayor a 99...
-      {
-        contador=0;//el contador se reinicia
+        contador = 0;  // El contador se reinicia
       }
     }
-    mostrarContador(primo);//Muestra en los visualizadores, el número guardado por la variable primo 
   }
 }
 
@@ -135,11 +121,11 @@ void loop()
 fin de evitar que al mantener el botón se modifique infinitamente el contador)*/
 
 int teclaPresionada(void)
-{  
+{
   // el valor del pulsador se establece según las variables:
   suma = digitalRead(PULSADOR_SUMA);
   resta = digitalRead(PULSADOR_RESTA);
-  
+
   if (suma == 1)//Si PULSADOR_SUMA no se presionó
   {
     suma_anterior = 1;//suma_anterior guarda el estado del pulsador para ser comparado
@@ -170,13 +156,13 @@ int teclaPresionada(void)
 //declaro una función para apagar todos los leds de los visualizadores
 void apagarDisplay()
 {
-    digitalWrite(A, LOW);
-    digitalWrite(B, LOW);
-    digitalWrite(C, LOW);
-    digitalWrite(D, LOW);
-    digitalWrite(E, LOW);
-    digitalWrite(F, LOW);
-    digitalWrite(G, LOW);
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
 }
 
 //------------------------------------
@@ -194,7 +180,7 @@ void mostrarNumero(int numero)
       digitalWrite(E, HIGH);
       digitalWrite(F, HIGH);
       break;
-        
+
     case 1:
       digitalWrite(B, HIGH);
       digitalWrite(C, HIGH);
@@ -264,46 +250,49 @@ void mostrarNumero(int numero)
       digitalWrite(F, HIGH);
       digitalWrite(G, HIGH);
       break;
-    }
+  }
 }
 
 //------------------------------------
-//declaro una función para determinar primos
-bool detectarPrimo(int numero)
+//declaro una función para mostrar múltiplos de 5
+void mostrarMultiplos5(int contador)
 {
-    bool esPrimo = true;
-    for (int i = 2; i < numero; i++) 
-    {
-      if (numero % i == 0)
-      {
-        return false;
-        break;//como encontró un divisor adicional, el número no es un número primo
-      } 
-    }
-  return esPrimo;//si retorna, el número si es un número primo
+  // Apago los visualizadores
+  prendeDigito(APAGADOS);
+
+  // Obtengo el múltiplo de 5 más cercano al contador
+  int multiplo5 = (contador / 5) * 5;
+
+  // Muestro el múltiplo de 5 en la unidad
+  mostrarNumero(multiplo5 % 10);
+  prendeDigito(UNIDAD);
+
+  // Muestro la decena como 0
+  mostrarNumero(0);
+  prendeDigito(DECENA);
 }
 
 //------------------------------------
 //declaro una función para poder controlar la activación de los visualizadores
 void prendeDigito(int digito)
 {
-	if (digito == UNIDAD)//Si el valor del argumento es "UNIDAD"
-    {
-    	digitalWrite(UNIDAD, LOW);//prendo el visualizador de la unidad
-        digitalWrite(DECENA, HIGH);//apago el visualizador de la decena
-      	delay(ANTIREBOTE);
-    }
-  	else if(digito == DECENA)//Si el valor del argumento es "DECENA"
-    {
-    	digitalWrite(UNIDAD, HIGH);//apago el visualizador de la unidad
-        digitalWrite(DECENA, LOW);//prendo el visualizador de la decena
-      	delay(ANTIREBOTE);
-    }
-  	else//Si el valor del argumento es "APAGADOS"
-    {
-    	digitalWrite(UNIDAD, HIGH);//apago los 2
-        digitalWrite(DECENA, HIGH); 
-    }
+  if (digito == UNIDAD)//Si el valor del argumento es "UNIDAD"
+  {
+    digitalWrite(UNIDAD, LOW);//prendo el visualizador de la unidad
+    digitalWrite(DECENA, HIGH);//apago el visualizador de la decena
+    delay(ANTIREBOTE);
+  }
+  else if(digito == DECENA)//Si el valor del argumento es "DECENA"
+  {
+    digitalWrite(UNIDAD, HIGH);//apago el visualizador de la unidad
+    digitalWrite(DECENA, LOW);//prendo el visualizador de la decena
+    delay(ANTIREBOTE);
+  }
+  else//Si el valor del argumento es "APAGADOS"
+  {
+    digitalWrite(UNIDAD, HIGH);//apago los 2
+    digitalWrite(DECENA, HIGH);
+  }
 }
 
 //------------------------------------
@@ -335,7 +324,7 @@ void mostrarAngulo()
   }
   if(lectura_flexion==236)//0°
   {
-  	angulo=0-2;
+    angulo=0-2;
   }
   if(lectura_flexion>238 && lectura_flexion<244)//5°
   {
@@ -400,7 +389,7 @@ void mostrarAngulo()
   if(lectura_flexion>=632)//179°
   {
     angulo=angulo-1;  
-  } 
+  }
   if(lectura_flexion==634)//180°
   {
     angulo=angulo+7;
@@ -412,13 +401,13 @@ void mostrarAngulo()
     Serial.print(angulo);
     Serial.print(" grados");
     Serial.println();  
-	angulo_anterior = angulo;// angulo_anterior guarda el ultimo valor del angulo    
+    angulo_anterior = angulo;// angulo_anterior guarda el ultimo valor del angulo    
   }
 }
 
 void controlarLuz()
 {
-  lectura_luz = analogRead(FOTODIODO);//guarda los valores analogicos del fotodiodo
+  lectura_luz = analogRead(SENSOR_LUZ);//guarda los valores analogicos del fotodiodo
   luz = map(lectura_luz, 49, 1023, 0, 100);//configuro los valores analogicos minimos y maximos para crear un rango de 0 a 100.
   if(luz != luz_anterior)///Si el valor de luz es distinto de luz_anterior...
   {
